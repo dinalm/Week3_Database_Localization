fdc9cc72ea87cc1cbdbba219aed1f482d60d6d8c
Dinal Maha Vidanelage <dinalm@metropolia.fi>
2026-04-07 02:58:35 +0300

    Replace dotenv with system ev

--- src/main/java/com/shoppingcart/DatabaseConnection.java
+++ src/main/java/com/shoppingcart/DatabaseConnection.java
@@ -1,16 +1,14 @@
 package com.shoppingcart;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final Dotenv dotenv = Dotenv.load();
-    private static final String URL = dotenv.get("DB_URL");
-    private static final String USER = dotenv.get("DB_USER");
-    private static final String PASSWORD = dotenv.get("DB_PASSWORD");
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
